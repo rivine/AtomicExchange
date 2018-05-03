7d67e67ebaf73f348b3b3d28333355c774795ed9
Jonas Delrue <jonas.delrue@artilium.com>
2018-05-03 20:46:45 +0200

    adding more flow

--- exchangeNodes/acceptor.py
+++ exchangeNodes/acceptor.py
@@ -7,14 +7,57 @@ import grpc
 import atomicswap_pb2
 import atomicswap_pb2_grpc
 
+import os
+from optparse import OptionParser
+import json
+from collections import namedtuple
+
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+
+
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+def execute(process):
+    process = os.popen(process)
+    output = reprocessed = process.read()
+    process.close()
+    return output.rstrip()
 
 class AtomicSwap(atomicswap_pb2_grpc.AtomicSwapServicer):
 
-    def AcceptInitiate(self, request, context):
-        return atomicswap_pb2.AcceptorSmartContractCreated(hash='2427128964')
+    
 
+    def ProcessInitiate(self, request, context):
+        global bitcoinaddress
+        print("Acceptor: processInitiated")
+        print(request.initiator_amount)
+        bitcoinaddress = execute('echo 123456789'.rstrip()) #bitcoin-cli gentnewaddress "" legacy
+        print("Acceptor: bitcoin address: ")
+        print(bitcoinaddress)
+        return atomicswap_pb2.InitiateReply(acceptor_address=bitcoinaddress) #if(initiator_amount == request.initiator_amount and acceptor_amount == request.initiator_amount):
+
+        return False
+    
+    def ProcessInitiateSwap(self, request, context):
+        print("processInitiateSwap")
+        print(execute('echo {\\"lockTime\\" : \\"48\\", \\"contractValue\\" : \\"5678\\", \\"recipientAddress\\" : \\"123456789\\"}'))
+        btc_audit_json =  execute('echo {\\"lockTime\\" : \\"48\\", \\"contractValue\\" : \\"5678\\", \\"recipientAddress\\" : \\"123456789\\"}')#execute('btcatomicswap --testnet auditcontract {} {}'.format(request.contract, request.transaction))
+        btc_audit = json2obj(btc_audit_json)
+        print("{} > {}".format(btc_audit.lockTime, 40))
+        print("{} = {}".format(btc_audit.contractValue, initiator_amount))
+        print("{} = {}".format(btc_audit.recipientAddress, bitcoinaddress))
+        if(int(btc_audit.lockTime) < 40 or btc_audit.contractValue != initiator_amount or btc_audit.recipientAddress != bitcoinaddress):
+            print("Contract invalid")
+            return False
+
+        return atomicswap_pb2.AcceptSwap()
+
+    def ProcessRedeemed(self,request,context):
+        print("ProcessRedeemed")
+        return atomicswap_pb2.RedeemFinished(finished=True)
+        
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -27,6 +70,24 @@ def serve():
     except KeyboardInterrupt:
         server.stop(0)
 
+initiator_amount = 0
+acceptor_amount = 0
+bitcoinaddress = ""
 
 if __name__ == '__main__':
+
+    parser = OptionParser()
+
+    parser.add_option("-m", "--my-amount", dest="acceptor_amount",
+                    help="Your amount of your currency to swap", metavar="INITIATORAMOUNT")
+
+    parser.add_option("-o", "--other-amount",
+                    dest="initiator_amount", default=True,
+                    help="The amount of the other partners currency to swap")
+    
+    (options, args) = parser.parse_args()
+    
+    initiator_amount = options.initiator_amount
+    acceptor_amount = options.acceptor_amount
+
     serve()
--- exchangeNodes/initiator.py
+++ exchangeNodes/initiator.py
@@ -6,17 +6,65 @@ import grpc
 
 import atomicswap_pb2
 import atomicswap_pb2_grpc
+from optparse import OptionParser
+
+import os
+import json
+from collections import namedtuple
+
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 
-def run():
+def execute(process):
+    process = os.popen(process)
+    output = reprocessed = process.read()
+    process.close()
+    return output
+
+
+def run(initiator_amount, acceptor_amount):
     channel = grpc.insecure_channel('localhost:50051')
     stub = atomicswap_pb2_grpc.AtomicSwapStub(channel)
-    response = stub.AcceptInitiate(atomicswap_pb2.InitiatorSmartContractCreated(hash='8341309506'))
 
-    print("AcceptorSmartContractReceived: " + response.hash)
 
+    response = stub.ProcessInitiate(atomicswap_pb2.Initiate(initiator_amount=initiator_amount, acceptor_amount=acceptor_amount))
+    #response.python -m pip install grpcio
+    print("Initiator: ")
+    print(response.acceptor_address)
+    #btc_atomicswap_json = Execute("btcatomicswap --testnet --rpcuser=user --rpcpass=pass initiate {} {}" % response.acceptor_address, initiator_amount)
+    #do atomic swap
+    print("btcatomicswap --testnet --rpcuser=user --rpcpass=pass initiate {} {}".format(response.acceptor_address, initiator_amount))
+
+    btc_atomicswap_json = "{ \"secret\" : \"12345789\", \"hash\" : \"abc12345678098\", \"contractfee\" : \"0.2\", \"refundfee\" : \"0.1\", \"contract\" : \"54345678\", \"contractTransaction\" : \"tx123456\" }"
+
+    print(btc_atomicswap_json)
+
+    btc_atomicswap = json2obj(btc_atomicswap_json)
+    
+    initiator_wallet_address =  execute("echo 099765432")  #`rivinec wallet address`
+    atomicswap_pb2.InitiateSwap(hash=btc_atomicswap.hash, contract=btc_atomicswap.contract, transaction=btc_atomicswap.contractTransaction, initiator_wallet_address=initiator_wallet_address)
+
+    response = stub.ProcessInitiateSwap(atomicswap_pb2.InitiateSwap())
+
+
+    response = stub.ProcessRedeemed(atomicswap_pb2.RedeemFinished())
+
+   
 
 
 
 if __name__ == '__main__':
-    run()
+
+    parser = OptionParser()
+
+    parser.add_option("-m", "--my-amount", dest="initiator_amount",
+                    help="Your amount of your currency to swap", metavar="INITIATORAMOUNT")
+
+    parser.add_option("-o", "--other-amount",
+                    dest="acceptor_amount", default=True,
+                    help="The amount of the other partners currency to swap")
+    
+    (options, args) = parser.parse_args()
+
+    run(float(options.initiator_amount), float(options.acceptor_amount))
