21f8d033d01c16d4bc0c39604cdfd94ca75c19f3
Jonas Delrue <jonas.delrue@artilium.com>
2018-05-17 11:19:06 +0200

    fix bug not joining ZT network

--- cryptoDocker/rundaemons.sh
+++ cryptoDocker/rundaemons.sh
@@ -2,13 +2,14 @@
 
 # Start the first process
 
-zerotier cli join c7c8172af1387fb0
+
 bitcoind -daemon
 
 # Start the second process
 tfchaind --network testnet -M cgtewb &
 
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+sleep 5
+zerotier-cli join c7c8172af1387fb0
 
-/bin/bash
-
+tail -f /dev/null
